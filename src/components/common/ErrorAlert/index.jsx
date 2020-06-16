dc9ae97aaa9c2218d084d3ca369416eb9a57bbbf
EvgenyLeukhiin <eugenel@effective.band>
2020-06-16 13:49:01 +0600

    Fix error console

--- src/components/common/ErrorAlert/index.jsx
+++ src/components/common/ErrorAlert/index.jsx
@@ -8,16 +8,15 @@ import isEmpty from 'lodash/isEmpty';
 import clearStorage from 'Utils/clearStorage';
 
 const ErrorAlert = ({ error }) => {
-  // eslint-disable-next-line no-console
-  console.log('ERROR: ', error.response);
-
   // redirect to login page if 401 error and clear localStorage
   if (error?.status === 401) {
     clearStorage();
     return <Redirect to='/login' />;
 
-  // show error message
+    // show error message
   } else {
+    // eslint-disable-next-line no-console
+    !isEmpty(error) && console.log('ERROR: ', error.response);
     return (
       <Alert color="danger" isOpen={!isEmpty(error)}>
         {`${error?.status} - ${error?.data?.error?.message}`}
